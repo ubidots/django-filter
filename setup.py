7b1d7f1bb1e153546f4055e68f1121c0f90a2f23
jdavidagudelo <jdaaa2009@gmail.com>
2024-02-02 11:31:28 -0500

    ⬆️ [HDK-8472] Django: updating django version.
    - Updating django to version 5.0.1.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 # FIXME: Main module requires django to be present, so cannot run setup.py in
 # clean environment.
 # from django_filters import __version__
-__version__ = "22.3.0"
+__version__ = "23.0.1"
 
 f = open("README.rst")
 readme = f.read()
@@ -58,6 +58,7 @@ setup(
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
+        "Framework :: Django :: 5.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
